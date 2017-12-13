7c7c60a932cd1d9bc6d0009b6a4494a13be23fd5
cdeister <cdeister@brown.edu>
2017-12-13 17:15:26 -0500

    v0.? changed stuff

--- comPortTester.m
+++ comPortTester.m
@@ -1,5 +1,5 @@
 %% open
-featherPath='/dev/cu.usbmodem3003971';
+featherPath='/dev/cu.usbmodem3099121';
 featherBaud=9600;
 feather=serial(featherPath,'BaudRate',featherBaud);
 fopen(feather);
@@ -17,6 +17,27 @@ fprintf(feather,'t9>');
 %% reset the variables
 
 fprintf(feather,'r1>');
+
+%%
+h=0;
+for h=1:1000000
+    if feather.BytesAvailable>0
+        tempBuf=fscanf(feather);
+        splitBuf=strsplit(tempBuf,',');
+        if numel(splitBuf)==2
+            disp(h)
+            eval([splitBuf{1} '=' num2str(splitBuf{2}) ';'])
+            disp(k)
+        elseif numel(splitBuf)~=2
+            disp("no")
+        end
+    else
+    end
+    
+    h=h+1;
+end
+
+
 
 %% close and clean up
 fclose(feather)
--- ptb_gabor.m
+++ ptb_gabor.m
@@ -1,79 +1,119 @@
-% %% open
-% featherPath='/dev/cu.usbmodem3003971';
-% featherBaud=9600;
-% feather=serial(featherPath,'BaudRate',featherBaud);
-% fopen(feather);
-% flushinput(feather);
+%% open com
+featherPath='/dev/cu.usbmodem3099121';
+featherBaud=9600;
+feather=serial(featherPath,'BaudRate',featherBaud);
+fopen(feather);
+flushinput(feather);
 
-%%
+% setup vars
 % Clear  the workspace
-clearvars -except orientList n numTrials freqList  
+% clearvars -except orientList n numTrials freqList  
 
 % Setup PTB with default values
 PsychDefaultSetup(2);
 
 % Set the screen number to the secondary monitor if there is one
-screenNumber = max(Screen('Screens'));
+screenNumber = max(Screen('Screens'));   
 
 % Define black, white and grey
 white = WhiteIndex(screenNumber);
 grey = white / 2; 
 
 % Skip sync tests
-Screen('Preference', 'SkipSyncTests', 2);
-
-% Open the screen
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey, [], 32, 2,...
-    [], [],  kPsychNeed32BPCFloat);
-
-%% 
-numTrials = 10;
-
-orientList = [0,20,40,80,120];
-freqList = [1,2,4,8,16,32];
+Screen('Preference', 'SkipSyncTests', 2); 
 
 %%
-for n = 1:numTrials
+% Open the screen
+[window, windowRect] = PsychImaging('OpenWindow',...
+    screenNumber, grey, [], 32, 2,[], [],kPsychNeed32BPCFloat);
+    
 
-% Dimension of the region of Gabor in pixels
-gaborDimPix = windowRect(4) / 2;
+numTrials = 1;                    
+   
+orientList = [0,20,40,80,120];
+freqList = [1,2,4,8,16,32];                                                           
+               
+k=0;
+lastK=k;    
+h=0;
+contrast = 0.0;
+orientation = 0;
+runningTask=1;
 
-% Sigma of Gaussian
-sigma = gaborDimPix / 7;
+% drain the buffer
+while feather.BytesAvailable>0
+    fscanf(feather);
+end
 
-% Gabor Parameters
-tempor = randi(numel(orientList));
-orientation = orientList(tempor);
-contrast = 0.8;
-aspectRatio = 1.0;
-phase = 0;
+while runningTask==1 || h<=10000
+    
+    if feather.BytesAvailable>0 
+        tempBuf=fscanf(feather);                                           
+        splitBuf=strsplit(tempBuf,',');
+        
+        if strcmp(splitBuf{1},'o')
+            orientation = str2num(splitBuf{2});
+        else
+        end
+        
+        if strcmp(splitBuf{1},'c')
+            contrast = str2num(splitBuf{2});
+        else
+        end
+        
+        if strcmp(splitBuf{1},'r')
+            runningTask = str2num(splitBuf{2});
+        else
+        end
+        
+    else 
+    end
+    
+    % Dimension of the region of Gabor in pixels
+    gaborDimPix = windowRect(4) / 2;
 
-% Spatial Frequency (Cycles Per Pixel)
-tempfreq = randi(numel(freqList));
-numCycles = freqList(tempfreq);
-freq = numCycles / gaborDimPix;
+    % Sigma of Gaussian
+    sigma = gaborDimPix / 7;
 
-% Build a procedural gabor texture 
-% https://groups.yahoo.com/neo/groups/psychtoolbox/conversations/topics/9174
-backgroundOffset = [0.5 0.5 0.5 0.0];
-disableNorm = 1;
-preContrastMultiplier = 0.5;
-gabortex = CreateProceduralGabor(window, gaborDimPix, gaborDimPix, [],...
-    backgroundOffset, disableNorm, preContrastMultiplier);
+    % Gabor Parameters
+    tempor = randi(numel(orientList));
+    
 
-% Randomise the phase of the Gabors and make a properties matrix.
-propertiesMat = [phase, freq, sigma, contrast, aspectRatio, 0, 0, 0];
+    aspectRatio = 1.0;
+    phase = 0;
 
-% Draw the Gabor
-Screen('DrawTextures', window, gabortex, [], [], orientation, [], [], [], [],...
-    kPsychDontDoRotation, propertiesMat');
+    % Spatial Frequency (Cycles Per Pixel)
+    tempfreq = 30;
+    numCycles = 10;
+    freq = numCycles / gaborDimPix;
 
-% Flip to the screen
-Screen('Flip', window);
+    backgroundOffset = [0.5 0.5 0.5 0.0];
+    disableNorm = 1;          
+    preContrastMultiplier = 0.5;
+    gabortex = CreateProceduralGabor(window, gaborDimPix, gaborDimPix, [],...
+        backgroundOffset, disableNorm, preContrastMultiplier);
 
-% Button press changes screen
-KbStrokeWait;
+    % Randomise the phase of the Gabors and make a properties matrix.
+    propertiesMat = [phase, freq, sigma, contrast, aspectRatio, 0, 0, 0];
+
+    % Draw the Gabor
+    Screen('DrawTextures', window, gabortex, [], [], orientation, [], [], [], [],...
+        kPsychDontDoRotation, propertiesMat');
+
+    % Flip to the screen
+    Screen('Flip', window);
+    h=h+1;
+%   KbStrokeWait;
 
 end
 
+
+
+
 sca;
+
+%%
+% close and clean up
+fclose(feather)
+delete(feather)
+clear all
